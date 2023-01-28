bdf47baf46f189b0a4a310bedebde29047468ed5
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-01-28 11:59:43 +0530

    setup & cleaning

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calculator = () => {
+    return (
+        <div className="m-5 bg-red-600">
+            <h2>hellow arre you ok ..</h2>
+        </div>
+    )
+}
+
+export default Calculator;
